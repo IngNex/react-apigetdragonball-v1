4a119849af360952399aaab2df7a93b0175d0756
IngNex <ingnexmaicol14@gmail.com>
2022-09-20 23:23:08 -0500

    Add globalstyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import GlobalStyles from './styles/GlobalStyles'
 import Home from './view/Home'
 
 export default function App() {
   return (
     <>
       <Home/>
+      <GlobalStyles/>
     </>
   )
 }
